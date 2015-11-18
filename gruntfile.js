fddda85887e2d3952a4ef8f86f16d9a7b6c5d168
NathanR <dnr500@gmail.com>
2015-11-18 20:35:39 +0000

    changes made to gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -11,8 +11,29 @@ module.exports = function(grunt) {
                 ]
 
             }
+        },
+        plato: {
+            src: {
+                options : {
+                    exclude: /\.min\.js$/
+                },
+                files: {
+                    'reports/plato': ['static/script/**/*.js']
+                }
+            }
+        },
+        open : {
+            plato : {
+                path: 'reports/plato/index.html',
+                app: 'Google Chrome'
+            }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-plato');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.registerTask('launch-plato', ['plato', 'open:plato']);
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
